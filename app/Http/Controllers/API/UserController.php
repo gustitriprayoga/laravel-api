0d7affe2a6d409276b6b6a115179259ace1975d4
TukS <gustitriprayoga18@gmail.com>
2023-12-11 09:06:02 +0700

    Initial commit

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    public function show($id)
+    {
+        $user = User::find($id);
+        return response()->json($user);
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::create($request->all());
+        return response()->json($user, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->update($request->all());
+        return response()->json($user);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::find($id);
+        $user->delete();
+        return response()->json(null, 204);
+    }
+}
